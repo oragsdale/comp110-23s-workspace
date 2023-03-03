56ef797bad46862c5f924f7c50acfe06fed54fbb
Owen Ragsdale <orag@ad.unc.edu>
2023-03-03 14:07:24 -0500

    cq2

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,10 +1,10 @@
 """Example function for unit tests"""
 
-def sum(xs: list[float]) -> float:
+__author__ = "730560351"
+
+def sum_new(xs: list[float]) -> float:
     """return sum off all elements in xs"""
     sum_total: float = 0.0
-    idx = 0
-    while idx < len(xs):
-        sum_total += xs[idx]
-        idx += 1
+    for item in xs:
+        sum_total += item
     return sum_total
